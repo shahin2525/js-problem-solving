7879908cfde0674b25d10e8e3727d872d843aa9c
shahin2525 <shahin451999@gmail.com>
2024-03-16 13:39:44 -0700

    checked leap year

--- simple-problem.js
+++ simple-problem.js
@@ -5,7 +5,7 @@ const inchToFeet = (inch) => {
 
 const myInch = 100;
 const myFeet = inchToFeet(myInch);
-
+//
 const milesToKm = (miles) => {
   const km = miles * 1.609344;
   return km;
@@ -26,4 +26,17 @@ const peterNumberIsEven = (number) => {
 const petersNumber1 = 98;
 const petersNumber2 = 117;
 const petersNumber = peterNumberIsEven(petersNumber2);
-console.log(petersNumber);
+
+// leaf year
+const isLeapYear = (year) => {
+  const isLeapYear = year % 4;
+  if (isLeapYear === 0) {
+    return "leap year", true;
+  } else {
+    return "leap year", false;
+  }
+};
+
+const year = 2025;
+const leapYear = isLeapYear(year);
+// console.log(leapYear);
