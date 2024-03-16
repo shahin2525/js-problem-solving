24c3070ad923a47646584009843eff12514d20a4
shahin2525 <shahin451999@gmail.com>
2024-03-16 13:04:19 -0700

    miles and inch converted paractice

--- simple-problem.js
+++ simple-problem.js
@@ -5,4 +5,12 @@ const inchToFeet = (inch) => {
 
 const myInch = 100;
 const myFeet = inchToFeet(myInch);
-console.log(myFeet);
+
+const milesToKm = (miles) => {
+  const km = miles * 1.609344;
+  return km;
+};
+
+const myMilesDistance = 2;
+const myKmDistance = milesToKm(myMilesDistance);
+console.log(myKmDistance);
