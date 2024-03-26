57b7c401dd4e63860d47174a6b8f002d8faf1963
shahin2525 <shahin451999@gmail.com>
2024-03-25 22:44:06 -0700

    practice string split and slice

--- js/search.js
+++ js/search.js
@@ -10,4 +10,10 @@ const doesExists = lyrics
 // }
 
 // console.log(lyrics.startsWith(searchString));
-console.log(lyrics.endsWith(searchString));
+// console.log(lyrics.endsWith(searchString));
+const parts1 = lyrics.split("");
+const parts2 = lyrics.split(" ");
+const parts3 = lyrics.split(".");
+const parts4 = lyrics.slice(5, 8);
+const parts5 = lyrics.substring(5, 8);
+console.log(parts5);
