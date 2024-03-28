d5b0ca02b482d37c2c38168001bc5f94feeedacb
shahin2525 <shahin451999@gmail.com>
2024-03-27 23:52:47 -0700

    practice celi and floor and abs number

--- js/math.js
+++ js/math.js
@@ -1,8 +1,14 @@
 const num1 = 25;
 const num2 = 35;
+const num3 = 2.1;
+const num4 = 2.9;
+const num5 = 2.4;
+const num6 = 2.5;
 
 const gap = Math.abs(num1 - num2);
-console.log(gap);
+const round = Math.ceil(num3);
+const floor = Math.floor(num4);
+console.log(floor);
 if (gap < 5) {
   console.log("i will marry you");
 } else {
