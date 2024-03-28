92d0644d5092dd49cdc60559c8c3bcbf4fd7d17b
shahin2525 <shahin451999@gmail.com>
2024-03-27 23:48:16 -0700

    practice math .abs

---
+++ js/math.js
@@ -0,0 +1,10 @@
+const num1 = 25;
+const num2 = 35;
+
+const gap = Math.abs(num1 - num2);
+console.log(gap);
+if (gap < 5) {
+  console.log("i will marry you");
+} else {
+  console.log("i will marry you");
+}
