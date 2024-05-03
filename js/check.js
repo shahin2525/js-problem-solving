bfdf0fa7a2861967ea1c9142605ada41d91001c0
shahin2525 <shahin451999@gmail.com>
2024-05-02 23:33:56 -0700

    for loop practice

---
+++ js/check.js
@@ -0,0 +1,23 @@
+// let x = 1;
+// while (x <= 19) {
+//   console.log(x);
+//   x = x + 2;
+// }
+
+// const arr = [10, 20, 30, 40];
+// arr[3] = 50;
+// arr.push(22);
+// arr.push(24);
+// arr.pop();
+
+// console.log(arr);
+
+arr2 = [10, 20, 30, 40, 44];
+// for (let age of arr2) {
+//   console.log(age);
+// }
+
+for (let i = 0; i < arr2.length; i++) {
+  const age = arr2[i];
+  console.log(age);
+}
