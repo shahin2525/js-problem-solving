f2041c7ce1301985773ee81fbc252d8938e01c5d
shahin2525 <shahin451999@gmail.com>
2024-05-04 23:10:54 -0700

    practice array duplicate

--- p-solving/array.js
+++ p-solving/array.js
@@ -9,9 +9,25 @@
 // const totalFriends = friends.concat(friends2);
 // console.log(totalFriends);
 
-const friends = [10, 20, 30, 40, 50];
-const partial1 = friends.slice(1, 4);
-// console.log(partial1);
-const partial2 = friends.splice(1, 2, "rakib", "tamim");
-console.log(partial2);
-console.log(friends);
+// const friends = [10, 20, 30, 40, 50];
+// const partial1 = friends.slice(1, 4);
+// // console.log(partial1);
+// const partial2 = friends.splice(1, 2, "rakib", "tamim");
+// console.log(partial2);
+// console.log(friends);
+
+const numbers = [10, 20, 30, 10, 20, 30, 2, 3];
+
+const checkDuplicate = (numbers) => {
+  let unique = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    if (unique.includes(number) === false) {
+      unique.push(number);
+    }
+  }
+  return unique;
+};
+
+const duplicate = checkDuplicate(numbers);
+console.log(duplicate);
