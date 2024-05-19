a8fdb692ff15f2b9ec5f1f66987dc0143dd806fc
shahin2525 <shahin451999@gmail.com>
2024-05-18 18:41:21 -0700

    practice class 4.9

---
+++ p-solving/last.js
@@ -0,0 +1,19 @@
+// function feetToInch(feet) {
+//   const inch = feet * 12;
+//   return inch;
+// }
+// const inch = feetToInch(10);
+// console.log(inch);
+
+//
+
+// function centimeterToMeter(centimeter) {
+//   const meter = centimeter / 100;
+//   return meter;
+// }
+// const meter = centimeterToMeter(200);
+// console.log(meter);
+
+//
+
+const paperRequirements = () => {};
