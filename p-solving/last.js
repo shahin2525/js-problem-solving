54e3f3d3cd17e656bbcb2f7d82dd61dde2180368
shahin2525 <shahin451999@gmail.com>
2024-05-18 20:17:01 -0700

    practice class 4.9-2

--- p-solving/last.js
+++ p-solving/last.js
@@ -16,4 +16,58 @@
 
 //
 
-const paperRequirements = () => {};
+// const paperRequirements = (firstBook, secondBook, thirdBook) => {
+//   const firstBookPage = 100;
+//   const secondBookPage = 200;
+//   const thirdBookPage = 300;
+//   const firstBookTotalPage = firstBook * firstBookPage;
+//   const secondBookTotalPage = secondBook * secondBookPage;
+//   const thirdBookTotalPage = thirdBook * thirdBookPage;
+//   const totalBookPage =
+//     firstBookTotalPage + secondBookTotalPage + thirdBookTotalPage;
+//   return totalBookPage;
+// };
+// const firstBookFigure = 2;
+// const secondBookFigure = 3;
+// const thirdBookFigure = 4;
+
+// const TotalPapers = paperRequirements(
+//   firstBookFigure,
+//   secondBookFigure,
+//   thirdBookFigure
+// );
+// console.log(TotalPapers);
+
+// const bestFriend = (friends) => {
+//   let i = 0;
+//   let maxName = [0];
+//   while (i < friends.length) {
+//     const name = friends[i];
+//     // console.log(name);
+//     if (name.length > maxName.length) {
+//       maxName = name;
+//     }
+//     i++;
+//   }
+
+//   return maxName;
+// };
+// const friends = ["rakib", "tamima", "rofiqul", "eiameen munshi"];
+// const bestFriendName = bestFriend(friends);
+// console.log(bestFriendName);
+
+const onlyPositive = (numbers) => {
+  let positive = [];
+  for (i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    // console.log(number);
+    if (number < 0) {
+      break;
+    }
+    positive.push(number);
+  }
+  return positive;
+};
+const numbers = [10, 5, 20, 25, -5, 16, 18];
+const getPositive = onlyPositive(numbers);
+console.log(getPositive);
