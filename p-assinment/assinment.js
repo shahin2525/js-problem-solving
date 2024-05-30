d2e592fe43559ad3b27910e83525497f14825606
shahin2525 <shahin451999@gmail.com>
2024-05-30 14:41:39 -0700

    practice 2 problem

---
+++ p-assinment/assinment.js
@@ -0,0 +1,22 @@
+// function cubeNumber(number) {
+//   if (isNaN(number)) {
+//     return "provide a number";
+//   }
+//   const result = number * number * number;
+//   return result;
+// }
+// const getCube = cubeNumber("asss");
+// console.log(getCube);
+
+// function matchFinder(ast1, ast2) {
+//   //   if (typeof ast1 !== "string" && typeof ast2 !== "string") {
+//   //     return "provide string";
+//   //   }
+//   if (typeof ast1 !== "string" || typeof ast2 !== "string") {
+//     return "please provide string inputs";
+//   }
+//   const result = ast1.includes(ast2);
+//   return result;
+// }
+// const getSting = matchFinder(12, "ohn");
+// console.log(getSting);
