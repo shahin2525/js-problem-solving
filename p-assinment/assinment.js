edbc86d2cf6b8994f9459adf8c1627865cf45695
shahin2525 <shahin451999@gmail.com>
2024-05-31 22:11:22 -0700

    practice 2

--- p-assinment/assinment.js
+++ p-assinment/assinment.js
@@ -20,3 +20,59 @@
 // }
 // const getSting = matchFinder(12, "ohn");
 // console.log(getSting);
+
+// function sortMaker(arr) {
+//   //   let arrElements = [];
+//   //   for (i = 0; i < arr.length; i++) {
+//   //     const element = arr[i];
+//   //     // console.log(element);
+//   //     if (element < 0) {
+//   //       return "invalid";
+//   //     }
+//   //     if (arr.indexOf(element) !== arr.lastIndexOf(element)) {
+//   //       return "equal";
+//   //     }
+
+//   //     arrElements.push(element);
+//   //   }
+//   //   arrElements.sort((a, b) => b - a);
+//   //   return arrElements;
+//   let positiveElement = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     if (element < 0) {
+//       return "invalid";
+//     }
+//     if (arr.indexOf(element) !== arr.lastIndexOf(element)) {
+//       return "equal";
+//     }
+//     positiveElement.push(element);
+//   }
+//   positiveElement.sort((a, b) => b - a);
+//   return positiveElement;
+// }
+// const arr = [3, 3];
+// const getSortMaker = sortMaker(arr);
+// console.log(getSortMaker);
+
+const findAddress = (obj) => {
+  const aspectedProperty = ["street", "house", "society"];
+
+  let objValues = [];
+  for (let i = 0; i < aspectedProperty.length; i++) {
+    const prop = aspectedProperty[i];
+    if (obj.hasOwnProperty(prop)) {
+      objValues.push(obj[prop]);
+    } else {
+      objValues.push("--");
+    }
+  }
+  return objValues.join(", ");
+};
+const myAddress = {
+  street: 10,
+  house: "15A",
+  society: "Earth Perfect",
+};
+const getAddress = findAddress(myAddress);
+console.log(getAddress);
